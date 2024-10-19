009aaff9f01316181a068df679e8e9e975a1e5d6
ShriramUmashankar <132140196+ShriramUmashankar@users.noreply.github.com>
2024-10-19 17:36:54 +0530

    Updated GetGravity.m
    Added potential function gradient as well as other terms and finished modelling gravity vector

--- GetGravity.m
+++ GetGravity.m
@@ -1,6 +1,32 @@
-function[g] = GetGravity(r)
-    L = 2*atan(r(2)/r(1)); % Check this very doubtful
-    g=[0,4,5]; % simply put for now
+function[g] = GetGravity(r,v,w)
+   
+    dist = norm(r);
+    la = 1000;
+    lb = 500;
+    lc = 250;
+    
+    volume = (4/3)*pi*la*lb*lc ;
+    density = 3000;
+    GM = volume*density*6.67432*10^-(11);
+
+    C20 = -0.1125;
+    C22 = 0.0375;
+    phi = atan2(r(3)/250,sqrt((r(2)/500)^2 + (r(1)/1000)^2));
+    lambda = atan2(r(2)/500,r(1)/1000);
+
+    
+    a_r = (-GM/dist^2) - ((3*GM*la^2)/(dist^4))*((C20/2)*(3*sin(phi)*sin(phi) -1) + 3*C22*cos(phi)*cos(phi)*cos(2*lambda));
+    a_phi = (GM*la^2/dist^3)*((3*C20*sin(phi)*cos(phi)) - 6*C22*sin(phi)*cos(phi)*cos(2*lambda));
+    a_lambda = (GM*la^2/dist^3)*(-6*C22*cos(phi)*cos(phi)*sin(2*lambda));
+    
+
+    T_Matrix = [cos(phi)*cos(lambda), -sin(phi)*sin(lambda), -sin(lambda) ;...
+        cos(phi)*sin(lambda), - sin(phi)*sin(lambda), cos(lambda);...
+        sin(phi), cos(phi), 0];
+
+
+    a_geo = transpose(T_Matrix*[a_r ; a_phi ; a_lambda]);
+
+    a_var = -2*(cross(w,v)) -cross(w,cross(w,r));
+    g=a_geo + a_var; % simply put for now
 end
-
-
