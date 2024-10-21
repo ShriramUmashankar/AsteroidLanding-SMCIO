df7d7494f157b6d288f2bae1612d20cb77362dd3
ShriramUmashankar <132140196+ShriramUmashankar@users.noreply.github.com>
2024-10-21 18:46:27 +0530

    Updated utilities.m
    Final Working Utilities file

--- utilities.m
+++ utilities.m
@@ -2,13 +2,13 @@ clc; close all; clear
 %% Define all constants here
 
 a = [0, 0, 0];
-T_max = 80;
+T_max = 30;
 m_0 = 1400; 
 c = 2206; % T = -m_dot * c
 w = [0, 0, 2.18 * 10^(-4)]; %Omega value
 v_fd = [0,0,0]; % Final velocity
 e_e = 0.0001; % epsilon, sliding mode control parameter
-k_e = 0.006; % exponential reaching law parameter
+k_e = 0.00225; % exponential reaching law parameter
 
 % 1 + b + c = 10
 b_x = 9; b_y = 8; b_z = 5;
@@ -16,15 +16,11 @@ c_x = 0; c_y = 1; c_z = 4;
 
 h=0.05 ; % Time step
 
-%% Landing Scenario / Initial Conditions
-
+%% Landing Scenario 1
 r_0 = [1389.37, -60.78, -30.5];
 r_fd = [1000, 0, 0];
+v_0 = [-2, -1.25, -0.5];
+
+%%
 R = norm(r_fd - r_0);
-v_0 = [-2, -1.25, -0.5];
-
-%%
-v_0 = [-2, -1.25, -0.5];
-
-%%
 
